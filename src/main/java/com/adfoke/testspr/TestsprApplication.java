73ba0cbff21df36b58bab51bb417a8dc52a8fc5d
adfoke <trawow@gmail.com>
2025-04-28 03:49:04 +0800

    first commit

---
+++ src/main/java/com/adfoke/testspr/TestsprApplication.java
@@ -0,0 +1,13 @@
+package com.adfoke.testspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adfoke/testspr/TestsprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adfoke.testspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
